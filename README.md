a4f821def18ef21ba61d5d660a5aa0fad1b64445
KalibrX <devinjstokes@gmail.com>
2016-11-12 02:22:44 -0800

    Added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+**TorrentBeam**
+
+    TorrentBeam is a proof of concept for myself to create a configurable, multi-torrent site search result aggregator.
+    It is currently in the testing / proof of concept phase.
--- search_result.py
+++ search_result.py
@@ -1,6 +1,12 @@
+import json
+
+
 class SearchResult:
     def __init__(self, title, link, added, seeds):
         self.title = title
         self.link = link
         self.added = added
         self.seeds = seeds
+
+    def toJSON(self):
+        return {"title": str(self.title), "link": str(self.link), "added": str(self.added), "seeds": str(self.seeds)}
